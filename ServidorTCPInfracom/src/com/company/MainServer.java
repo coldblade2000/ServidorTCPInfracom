7b764603b375775c819c4b73d8a239788fa9919d
coldblade2000 <diegogranada7@gmail.com>
2022-03-05 17:48:01 -0500

    Acabe la mayoria del servidor, faltan unas cositas

---
+++ ServidorTCPInfracom/src/com/company/MainServer.java
@@ -0,0 +1,146 @@
+package com.company;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.concurrent.CyclicBarrier;
+
+
+public class MainServer {
+
+    // https://www.baeldung.com/a-guide-to-java-sockets
+    private static BufferedWriter logBW;
+    private static FileWriter logFW;
+    public LinkedList<ThreadServer> threads;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private String filepath;
+    private String hash;
+
+    //TODO Calcular tiempo de transferencia
+    //TODO Recibir verificacion de que el archivo llego bien
+    //TODO Handle cuando se acaba de mandar todos los archivos
+    //TODO
+
+
+    public MainServer(int archivo) {
+        if (archivo == 1) {
+            filepath = "Archivos/pequenio.mp4";
+        } else {
+            filepath = "Archivos/grande.mp4";
+        }
+        try {
+            //Calculate hash of selected file
+            File file = new File(filepath);
+            MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+            hash = getFileChecksum(md5Digest, file);
+            log("El archivo seleccionado es el de " + (archivo == 1 ? "100MB" : "250MB"));
+            log("El hash del archivo seleccionado es: " + hash);
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        LocalDateTime now = LocalDateTime.now();
+        String filename = dtf.format(now) + ".txt";
+        MainServer.logFW = new FileWriter(filename);
+        MainServer.logBW = new BufferedWriter(MainServer.logFW);
+        Scanner scan = new Scanner(System.in);
+
+        int clientes;
+        int archivo;
+        while (true) {
+            System.out.println("Cuantos clientes quieres que se conecten antes de enviar? ");
+            clientes = scan.nextInt();
+            System.out.println("Cual archivo quieres subir? 1 para el de 100MB o 2 para el de 250MB: ");
+            archivo = scan.nextInt();
+            if (clientes < 1 || archivo < 1 || archivo > 2) {
+                MainServer.log("Error, opciones invalidas");
+            } else {
+                break;
+            }
+        }
+        log(String.format("Comenzando servidor del archivo %s para %s clientes%n", archivo, clientes));
+        MainServer server = new MainServer(archivo);
+        ThreadServer.barrier = new CyclicBarrier(clientes);
+        try {
+            server.start(25505, clientes, archivo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void log(String log) {
+        System.out.println(log);
+        try {
+            MainServer.logBW.write(log + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //https://howtodoinjava.com/java/java-security/sha-md5-file-checksum-hash/
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
+        //Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+
+        //Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        //Read file data and update in message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+        ;
+
+        //close the stream; We don't need it now.
+        fis.close();
+
+        //Get the hash's bytes
+        byte[] bytes = digest.digest();
+
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //return complete hash
+        return sb.toString();
+    }
+
+    public void start(int port, int clientes, int archivo) throws IOException {
+        serverSocket = new ServerSocket(port);
+        threads = new LinkedList<>();
+        // Create new ThreadServer for every connection that arrives.
+        while (threads.size() < clientes) {
+            clientSocket = serverSocket.accept();
+            MainServer.log("New connection created!");
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+
+            ThreadServer connectionServer = new ThreadServer(filepath, hash, clientSocket, out, in, this);
+            threads.add(connectionServer);
+            connectionServer.start();
+
+        }
+
+    }
+
+    public void stop() throws IOException {
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+}
---
+++ ServidorTCPInfracom/src/com/company/ThreadServer.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class ThreadServer extends Thread {
+    public static CyclicBarrier barrier;
+    private final String hash;
+    private final Socket clientSocket;
+    private final DataOutputStream out;
+    private final DataInputStream in;
+    private final MainServer mainServer;
+    private String filepath;
+
+    public ThreadServer(String filepath, String hash, Socket clientSocket, DataOutputStream out, DataInputStream in, MainServer mainServer) {
+
+        this.filepath = filepath;
+        this.hash = hash;
+        this.clientSocket = clientSocket;
+        this.out = out;
+        this.in = in;
+        this.mainServer = mainServer;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            MainServer.log("Server started, there are now " + mainServer.threads.size() + " threads.");
+            out.writeUTF("Conection successful. Please send \"YES\" to confirm you're ready to receive file. ");
+            // Makes sure client confirms they are ready
+            while (true) {
+                try {
+                    if (in.readUTF().trim().equalsIgnoreCase("yes")) {
+                        break;
+                    } else {
+                        out.writeUTF("Invalid command, please sent \"YES\" to confirm you're ready to receive file. ");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                MainServer.log("Waiting for other connections to send file. Currently " + (1 + barrier.getNumberWaiting()) + " threads waiting to send file.");
+                out.writeUTF("Waiting for other connections to send file. Currently " + (1 + barrier.getNumberWaiting()) + " threads waiting to send file.");
+                // Barrier that waits until all clients have connected
+                barrier.await();
+                //Send file, all clients are ready
+                sendFile(filepath);
+
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+                //Remove thread from threadlist if the connection drops or something else happens
+                mainServer.threads.remove(this);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        //Write hash to output stream
+        out.writeUTF(hash);
+        // send file size
+        out.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            out.write(buffer,0,bytes);
+            out.flush();
+        }
+        fileInputStream.close();
+    }
+
+}
